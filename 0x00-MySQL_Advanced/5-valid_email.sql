fb9924bfe5eac7f8e2f6391cbae1c2a40ec16882
Julesntizimira <ntizimijules5@gmail.com>
2024-01-18 13:04:50 +0000

    Email validation to sent

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,6 +4,8 @@ CREATE TRIGGER valid
 AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-	SET NEW.valid_email = 0;
+    IF NEW.email != OLD.email THEN
+        UPDATE users SET valid_email = 0 WHERE email = NEW.email;
+    END IF;
 END $$
 DELIMITER ;
