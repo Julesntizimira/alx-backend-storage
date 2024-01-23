ee828e7488cfd717f7dcf3743c87b0f524c15f5e
Julesntizimira <ntizimijules5@gmail.com>
2024-01-23 12:49:46 +0200

    correct task9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,8 +6,5 @@ def insert_school(mongo_collection, **kwargs):
     '''a Python function that inserts a new document
        in a collection based on kwargs
     '''
-    mongo_collection.insert(kwargs)
-    key = list(kwargs.keys())[0]
-    val = kwargs.get(key)
-    id = mongo_collection.find_one({key: val}).get('_id')
+    id = mongo_collection.insert_one(kwargs).inserted_id
     return id
