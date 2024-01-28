683393bddd02831feb0eaa35b19f85008a19b3b7
Julesntizimira <ntizimijules5@gmail.com>
2024-01-28 11:24:56 +0200

    Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,7 @@ from functools import wraps
 from typing import Union, Callable, Any
 
 
+'''
 def call_history(method: Callable) -> Callable:
     """
     Counts the number of times a function is called
@@ -35,6 +36,38 @@ def call_history(method: Callable) -> Callable:
         self._redis.rpush(output, result)
         return result
     return wrapper_function_history
+'''
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Counts the number of times a function is called
+    Args:
+        method: The function to be decorated
+    Returns:
+        The decorated function
+    """
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function for the decorated function
+        Args:
+            self: The object instance
+            *args: The arguments passed to the function
+            **kwargs: The keyword arguments passed to the function
+        Returns:
+            The return value of the decorated function
+        """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(data))
+        return data
+
+    return wrapper
 
 
 def count_calls(method: Callable) -> Callable:
